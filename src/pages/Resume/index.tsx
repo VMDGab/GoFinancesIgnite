2e188e50a9c97300f611259040af54d964c7ebd9
Gabriel <gmatosdantas777@gmail.com>
2022-12-12 21:35:45 -0300

    À resolver erro no gráfico

--- src/pages/Resume/index.tsx
+++ src/pages/Resume/index.tsx
@@ -4,10 +4,11 @@ import {
   Header,
   TextHeader,
   CategoryList,
-  
+
 } from './styles';
 
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import { VictoryPie } from "victory-native";
 
 import { HistoryCard } from '../../components/HistoryCard';
 import { categories } from '../../../utils/categories';
@@ -19,45 +20,46 @@ interface TransactionData {
   amount: string
   category: string
   date: string
-  color:string
+  color: string
 }
 
 interface CategoryData {
   name: string
   total: number
   totalFormatted: string;
-  color:string  
+  color: string
 }
 export function Resume() {
 
   const [totalByCategories, setTotalByCategories] = useState<CategoryData[]>([])
-
+  const TransactionData = '@GoFinances:Cadastro';
+  
   async function loadData() {
-    const TransactionData = '@GoFinances:Cadastro';
+   
     const response = await AsyncStorage.getItem(TransactionData)
     const responseFormatted = response ? JSON.parse(response) : []
 
     const expensives = responseFormatted
       .filter((expensives: TransactionData) => expensives.type === 'negative');
 
-      const totalByCategory: CategoryData[] = [];
+    const totalByCategory: CategoryData[] = [];
 
-      categories.forEach(category => {
-        let categorySum = 0;
-  
-        expensives.forEach((expensive: TransactionData) => {
-          if(expensive.category === category.name){
-            categorySum += Number(expensive.amount);
-          }        
-        });
-  
-        if(categorySum > 0){
-          const totalFormatted = categorySum
+    categories.forEach(category => {
+      let categorySum = 0;
+
+      expensives.forEach((expensive: TransactionData) => {
+        if (expensive.category === category.name) {
+          categorySum += Number(expensive.amount);
+        }
+      });
+
+      if (categorySum > 0) {
+        const totalFormatted = categorySum
           .toLocaleString('pt-BR', {
             style: 'currency',
             currency: 'BRL'
           })
-  
+
 
         totalByCategory.push({
           name: category.name,
@@ -66,7 +68,8 @@ export function Resume() {
           color: category.color
         })
       }
-      setTotalByCategories(totalByCategory)
+
+      setTotalByCategories(totalByCategory);
     })
 
 
@@ -80,23 +83,35 @@ export function Resume() {
   useFocusEffect(
 
     useCallback(() => { loadData(); }, [])
-    
-    )
+
+  )
   return (
     <Container>
       <Header>
         <TextHeader>Resumo por categoria</TextHeader>
       </Header>
- 
-      <CategoryList 
-      data={totalByCategories}
-      renderItem={({item}) =>
 
-        <HistoryCard
-        title={item.name}
-        amount={item.totalFormatted}
-        color={item.color}
-      />}
+      <VictoryPie
+        data={totalByCategories}
+        x='name'
+        y='total'
+        innerRadius={50}
+        style={{
+        labels: {
+        fill: 'black', fontSize: 15, padding: 7,
+        }, }}
+
+      />
+
+      <CategoryList
+        data={totalByCategories}
+        renderItem={({ item }) =>
+
+          <HistoryCard
+            title={item.name}
+            amount={item.totalFormatted}
+            color={item.color}
+          />}
       />
 
     </Container>
