a6fdd8a0df0a9c885d2243400277b31a332eead6
Gabriel <gmatosdantas777@gmail.com>
2022-12-13 20:19:45 -0300

    Gráfico Finalizado

--- src/pages/Resume/index.tsx
+++ src/pages/Resume/index.tsx
@@ -4,15 +4,23 @@ import {
   Header,
   TextHeader,
   CategoryList,
-
+  ChartContainer,
+  MonthSelect,
+  MonthSelectButton,
+  MonthSelectIcon,
+  Month,
+  LoadContainer
 } from './styles';
 
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { VictoryPie } from "victory-native";
-
+import { ActivityIndicator } from 'react-native';
 import { HistoryCard } from '../../components/HistoryCard';
 import { categories } from '../../../utils/categories';
 import { useFocusEffect } from '@react-navigation/native';
+import { useTheme } from 'styled-components';
+import {addMonths, subMonths, format} from 'date-fns';
+import { ptBR } from 'date-fns/locale';
 
 interface TransactionData {
   type: 'positive' | 'negative'
@@ -24,23 +32,47 @@ interface TransactionData {
 }
 
 interface CategoryData {
-  name: string
-  total: number
+  key: string;
+  name: string;
+  total: number;
   totalFormatted: string;
-  color: string
+  color: string;
+  percent: string;
 }
+
 export function Resume() {
 
-  const [totalByCategories, setTotalByCategories] = useState<CategoryData[]>([])
+  const [isLoading, setIsLoading] = useState(false);
+  const [selectedDate, setSelectedDate] = useState(new Date());
+  const [totalByCategories, setTotalByCategories] = useState<CategoryData[]>([]);
+
+  const theme = useTheme();
+
   const TransactionData = '@GoFinances:Cadastro';
-  
+
+  function handlDateChange(action: 'next' | 'prev'){    
+    if(action === 'next'){
+      setSelectedDate(addMonths(selectedDate, 1));
+    }else{
+      setSelectedDate(subMonths(selectedDate, 1));
+    }
+  }
+
   async function loadData() {
-   
+  setIsLoading(true);
     const response = await AsyncStorage.getItem(TransactionData)
     const responseFormatted = response ? JSON.parse(response) : []
 
     const expensives = responseFormatted
-      .filter((expensives: TransactionData) => expensives.type === 'negative');
+      .filter((expensive: TransactionData) => 
+      expensive.type === 'negative' &&
+      new Date(expensive.date).getMonth() === selectedDate.getMonth() &&
+      new Date(expensive.date).getFullYear() === selectedDate.getFullYear());
+
+    const expensiveTotal = expensives.reduce((acumullator: number, expensive: TransactionData) => {
+      return acumullator + Number(expensive.amount)
+    }, 0)
+
 
     const totalByCategory: CategoryData[] = [];
 
@@ -60,26 +92,25 @@ export function Resume() {
             currency: 'BRL'
           })
 
+        const percent = `${(categorySum / expensiveTotal * 100).toFixed(0)}%`
 
         totalByCategory.push({
+          key: category.key,
           name: category.name,
-          total: categorySum,
+          color: category.color,
+          total: categorySum, 
           totalFormatted,
-          color: category.color
-        })
+          percent        
+        });
       }
 
       setTotalByCategories(totalByCategory);
+      setIsLoading(false);
     })
 
 
   }
 
-  useEffect(() => {
-
-    loadData()
-
-  }, [])
   useFocusEffect(
 
     useCallback(() => { loadData(); }, [])
@@ -90,19 +121,37 @@ export function Resume() {
       <Header>
         <TextHeader>Resumo por categoria</TextHeader>
       </Header>
+     
+      <MonthSelect>
+              <MonthSelectButton onPress={() => handlDateChange('prev')}>
+                <MonthSelectIcon name="chevron-left"/>
+              </MonthSelectButton>
 
-      <VictoryPie
-        data={totalByCategories}
-        x='name'
-        y='total'
-        innerRadius={50}
-        style={{
-        labels: {
-        fill: 'black', fontSize: 15, padding: 7,
-        }, }}
+              <Month>
+                { format(selectedDate, 'MMMM, yyyy', {locale: ptBR}) }
+              </Month>
 
-      />
+              <MonthSelectButton onPress={() => handlDateChange('next')}>
+                <MonthSelectIcon name="chevron-right"/>
+              </MonthSelectButton>
+            </MonthSelect>
 
+      <ChartContainer>
+        <VictoryPie
+           data={totalByCategories}
+          x='percent'
+          y='total'
+          colorScale={totalByCategories.map(category => category.color)}
+          style={{
+            labels: {
+              fontSize: 18,
+              fontWeight: 'bold',
+              fill: 'white'
+            }
+          }}
+          labelRadius={50}
+        />
+      </ChartContainer>
       <CategoryList
         data={totalByCategories}
         renderItem={({ item }) =>
--- src/pages/Resume/styles.ts
+++ src/pages/Resume/styles.ts
@@ -1,5 +1,7 @@
 import styled from 'styled-components/native';
 import { RFValue, RFPercentage } from 'react-native-responsive-fontsize';
+import {Feather} from '@expo/vector-icons'
+import { BorderlessButton } from 'react-native-gesture-handler';
 
 export const Container = styled.View`
   flex: 1;
@@ -20,6 +22,34 @@ font-family: ${({theme}) => theme.fonts.regular};
 `
 export const CategoryList = styled.FlatList`
 
-padding: 12px 24px
+padding: 12px 24px ;
 
 `
+export const ChartContainer = styled.View`
+
+width: 100%;
+align-items: center;
+`
+export const MonthSelect = styled.View`
+  width: 100%;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;  
+  margin-top: 24px;
+`;
+
+export const MonthSelectButton = styled.TouchableHighlight``;
+
+export const MonthSelectIcon = styled(Feather)`
+  font-size: ${RFValue(24)}px;
+`;
+
+export const Month = styled.Text`
+  font-family: ${({ theme }) => theme.fonts.regular};
+  font-size: ${RFValue(20)}px;
+`;
+export const LoadContainer = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+`;
